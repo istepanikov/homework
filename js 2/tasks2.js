adbe447d53ad99ac45f009f97d41b67a12512535
istepanikov <56891772+istepanikov@users.noreply.github.com>
2019-11-27 21:59:57 +0300

    Create tasks2.js

---
+++ js 2/tasks2.js
@@ -0,0 +1,9 @@
+let a , b , c, max;
+
+a = Math.floor(Math.random()* 100);
+b = Math.floor(Math.random()* 100);
+c = Math.floor(Math.random()* 100);
+max = (a + b + c); 
+
+
+alert (max*((a*b*c, a+b+c) +3));
